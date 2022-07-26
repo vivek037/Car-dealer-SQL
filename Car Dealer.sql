08645b40dbd4b848a0f5c86fa67e6643ce3195c7
Vivek Singh <69675422+vivek037@users.noreply.github.com>
2022-07-26 12:45:53 +0530

    Update Car Dealer.sql

--- Car Dealer.sql
+++ Car Dealer.sql
@@ -36,33 +36,37 @@ select '16188', 'Series 6', '30000' from dual union
 select '19182', 'Series 5', '25000' from dual union
 select '21199', 'Series 4', '20000' from dual;
 
-select CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name),Sum(Deposit_paid_for_booking) from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) group by Emp_number;
+select CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name),Sum(Deposit_paid_for_booking) 
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) group by Emp_number;
 
 select CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, 
-Sum(Deposit_paid_for_booking) as TOTAL_DEPOSITS from Sales_Team JOIN sales_data on (Emp_Number = Customer_manager_ID ) group by Emp_Number;
+Sum(Deposit_paid_for_booking) as TOTAL_DEPOSITS from Sales_Team 
+JOIN sales_data on (Emp_Number = Customer_manager_ID )
+group by Emp_Number;
 
-#question 1
+
 select 
 CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, 
 Sum(Car_Price) as TOTAL_SALES 
-from Sales_Team JOIN sales_data on (Emp_Number = Customer_manager_ID ) JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
+from Sales_Team JOIN sales_data on (Emp_Number = Customer_manager_ID )
+JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
 group by Emp_Number;
 
 select 
 CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, 
 Sum(Car_Price) as TOTAL_SALES,
 ((Monthly_target -  Sum(Car_Price))/Monthly_target)*100
-from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) JOIN Cars_Data on ( Customer_Car_Code = Car_Code );
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) 
+JOIN Cars_Data on ( Customer_Car_Code = Car_Code );
 
-# question 2
 select 
 CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, 
 Sum(Car_Price) as TOTAL_SALES,
 CONCAT(((Monthly_target -  Sum(Car_Price))/Monthly_target)*100,'%') 
-from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) 
+JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
 group by Emp_number,Monthly_target;
 
-#question 3
 select 
 Car_Name,Count(*) from sales_data JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
 group by Car_Code;
@@ -71,7 +75,7 @@ select
 Car_Name,Count(*) as frequency from sales_data JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
 group by Car_Code;
 
-#question 4
+
 select 
 CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as CAR_DEALER_FULL_NAME, 
 CUSTOMER_NAME,
@@ -79,15 +83,21 @@ CAR_NAME,
 CAR_PRICE,
 Deposit_paid_for_booking as DEPOSIT_GIVEN,
 (CAR_PRICE-Deposit_paid_for_booking) as AMOUNT_LEFT
-from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) JOIN Cars_Data on ( Customer_Car_Code = Car_Code );
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) 
+JOIN Cars_Data on ( Customer_Car_Code = Car_Code );
 
-#question 6
-select CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, MIN(Deposit_paid_for_booking) as MINIMUM_DEPOSITS from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) group by Emp_number;
 
-#question 5
+select CONCAT(Car_Dealer_First_Name,' ',Car_Dealer_Last_Name) as FULL_EMP_NAME, 
+MIN(Deposit_paid_for_booking) as MINIMUM_DEPOSITS 
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) 
+group by Emp_number;
+
+
 
 select 
 Car_Code,
 CONCAT((Sum(Car_Price)/Monthly_target)*100,'%') as percetage_each_car
-from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) JOIN Cars_Data on ( Customer_Car_Code = Car_Code ) where Emp_number = '12134'
+from Sales_Team JOIN sales_data on (Emp_number = Customer_manager_ID ) 
+JOIN Cars_Data on ( Customer_Car_Code = Car_Code )
+where Emp_number = '12134'
 group by Car_Code;
